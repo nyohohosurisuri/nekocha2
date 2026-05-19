c05bb440efcdcd6fe89ba2e6b26e0624eac73f84
nyohohosurisuri <mokomokobabababa@gmail.com>
2026-05-20 04:07:56 +0900

    Clarify local preview firewall prompt

--- ローカルで確認.bat
+++ ローカルで確認.bat
@@ -3,6 +3,7 @@ cd /d %~dp0
 echo ローカルサーバーを起動します...
 echo PCでは http://localhost:4173 を開いてください。
 echo iPhone SafariではGitHub Pagesではなく、同じWi-Fiで下のURL候補を開いてください。
+echo Windowsファイアウォールの確認が出た場合は、アクセスを許可してください。
 powershell -NoProfile -Command "Get-NetIPAddress -AddressFamily IPv4 | Where-Object { $_.IPAddress -notlike '127.*' -and $_.IPAddress -notlike '169.254.*' -and $_.PrefixOrigin -ne 'WellKnown' } | Sort-Object InterfaceAlias | ForEach-Object { '  ' + $_.InterfaceAlias + '  http://' + $_.IPAddress + ':4173' }"
 echo 終了するにはこの画面を閉じるか Ctrl+C を押してください。
 echo.
